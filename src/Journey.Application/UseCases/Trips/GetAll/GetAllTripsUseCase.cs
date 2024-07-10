7a5772f3d3de9ca7edce115bd10602049f26fd44
João Vitor <Joo Vitor@DESKTOP-ABOMC5E>
2024-07-10 00:22:27 -0300

    Adicionar arquivos de projeto.

---
+++ src/Journey.Application/UseCases/Trips/GetAll/GetAllTripsUseCase.cs
@@ -0,0 +1,28 @@
+﻿using Journey.Communication.Requests;
+using Journey.Communication.Responses;
+using Journey.Infrastructure;
+using Journey.Infrastructure.Entities;
+
+namespace Journey.Application.UseCases.Trips.GetAll
+{
+    public class GetAllTripsUseCase
+    {
+        public ResponseTripsJson Execute()
+        {
+            var dbContext = new JourneyDbContext();
+
+            var trips = dbContext.Trips.ToList();
+
+            return new ResponseTripsJson
+            {
+                Trips = trips.Select( i => new ResponseShortTripJson
+                {
+                    Id = i.Id,
+                    EndDate = i.EndDate,
+                    Name = i.Name,
+                    StartDate = i.StartDate
+                }).ToList()
+            };
+        }
+    }
+}
---
+++ src/Journey.Exception/ExceptionsBase/JourneyException.cs
@@ -0,0 +1,7 @@
+﻿namespace Journey.Exception.ExceptionsBase
+{
+    public class JourneyException : System.Exception
+    {
+        public JourneyException(string message) : base(message) { }
+    }
+}
